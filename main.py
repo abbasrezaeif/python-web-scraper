9e1a62f85d41972785dd5bc303d6d99a665af8ee
Abbas Rezaei <arf135@gmail.com>
2026-06-29 10:39:51 +0330

    Separate title extraction function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from scraper import download_page
+from scraper import get_title
 
 
 def main():
+
     url = "https://example.com"
 
     html = download_page(url)
 
-    print(html[:500])
+    title = get_title(html)
+
+    print(title)
 
 
 if __name__ == "__main__":
--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def download_page(url):
     response = requests.get(url)
-
     return response.text
+
+
+def get_title(html):
+    soup = BeautifulSoup(html, "html.parser")
+    return soup.title.text
