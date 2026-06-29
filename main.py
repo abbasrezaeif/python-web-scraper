f809045b2ffd1cdbb111cd6299f29880b5d789a2
Abbas Rezaei <arf135@gmail.com>
2026-06-29 10:47:04 +0330

    Extract links from webpage

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from scraper import download_page
-from scraper import get_title
+from scraper import download_page, get_title, get_links
 
 
 def main():
-
     url = "https://example.com"
 
     html = download_page(url)
 
     title = get_title(html)
+    links = get_links(html)
 
+    print("Page Title:")
     print(title)
+
+    print("\nLinks:")
+    for link in links:
+        print(f"- {link['text']}: {link['url']}")
 
 
 if __name__ == "__main__":
--- scraper.py
+++ scraper.py
@@ -10,3 +10,21 @@ def download_page(url):
 def get_title(html):
     soup = BeautifulSoup(html, "html.parser")
     return soup.title.text
+
+
+def get_links(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    links = []
+
+    for link in soup.find_all("a"):
+        href = link.get("href")
+        text = link.text.strip()
+
+        if href:
+            links.append({
+                "text": text,
+                "url": href
+            })
+
+    return links
